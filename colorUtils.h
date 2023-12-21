5a9b5dca1b13b458ea7fa3ab9aee81c15eecc7d2
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-12-21 21:18:53 +0500

    implementing rgbToCMYK(); function

--- colorUtils.h
+++ colorUtils.h
@@ -3,6 +3,7 @@ int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k);
 
 int cmykToRGB(double c, double m, double y, double k, int *r, int *g, int *b);
 
+int max();
 
 //cmykToRGB(); FUNCTION DEFINATION
 
@@ -28,6 +29,55 @@ return false;
 
 return true;
 
+}
+
+int max(int a,int b, int c){
+
+if(a>=b && a>=c){
+  return a;
+}
+
+else if( b>=a && b>=c){
+
+  return b;
 
 }
 
+return c;
+
+}
+
+int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k){
+
+double M = *m;
+
+double C = *c; 
+
+double Y = *y; 
+
+double K = *k; 
+
+
+r = r/255; 
+
+g = g/255;
+
+b = b/255;
+
+*k = 1 - ( max(r,g,b) ); 
+
+*c = (1- r- *k) / (1-*k);
+
+*m = (1 - g -*k) / (1-*k);
+
+*y = (1-b-*k) / (1-*k);
+
+if( (M == *m) || (C == *c) || (Y==*y) || (K==*k) ){
+
+return false;
+
+}
+
+return true;
+
+}
